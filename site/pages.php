47f858eb9862fbdac6a31b9b0ae019f314d60702
Iasmin Mendes <Iasmin Mendes>
2016-09-26 19:43:25 -0300

    Page to manage web pages

---
+++ site/pages.php
@@ -0,0 +1,96 @@
+<?php
+    require_once __DIR__ . "/class/autoload.php";
+
+    use \html\Page as Page;
+    use \html\Menu as Menu;
+    use \configuration\Globals as Globals;
+
+    Page::header(Globals::ENTERPRISE_NAME);
+
+    Menu::startMenu();
+        Menu::startItem();
+        Menu::addItem(PROJECT_ROOT . "#", "Páginas");
+            Menu::initSubItem();
+                Menu::addItem(PROJECT_ROOT . "category.php", "Gerenciar de Categoria");
+                Menu::addItem(PROJECT_ROOT . "newPage.php", "Nova Página");
+                Menu::addItem(PROJECT_ROOT . "pages.php", "Gerenciar Páginas");
+            Menu::endSubItem();
+        Menu::endItem();
+    Menu::endMenu();
+?>
+
+<!--Conteúdo da página-->
+<div id="delete"></div>
+<div id="content" style="text-align: left;">
+    <article>
+
+        <h1>Gerência de Páginas</h1>
+        <form>
+            <label>Páginas Atuais</label><br>
+            <select name="pages" id="select_page">
+              <?php include 'controller/findPage.php'; ?>
+            </select><br>
+            <input type="button" name="submit" value="Excluir" id="delete_button">
+        </form>
+
+    </article>
+</div>
+<?php
+    Page::footer();
+?>
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+    $('#delete_button').click(function(){
+        $.ajax({
+            url: 'controller/deletePage.php?code=' + $('#select_page').val() +
+                '&title=' + $('#select_page option:selected').text(),
+            success: function(data) {
+                alert(data);
+                $.ajax({
+                    url: 'controller/findWebPages.php',
+                    success: function(data){
+                        $('#select_page').html(data);
+                    }
+                });
+            },
+            beforeSend: function(){
+                $('#delete').html("Carregando...");
+            },
+            complete: function(){
+                $('#delete').html("");
+            },
+        });
+    });
+
+
+/*
+    $('#register_button').click(function(){
+        $.ajax({
+            url: 'controller/registerCategory.php?name=' + $('#new_category').val(),
+            success: function(data) {
+                alert(data);
+                $.ajax({
+                    url: 'controller/findCategory.php',
+                    success: function(data){
+                        $('#select_update').html(data);
+                        $('#new_category').val("");
+                    }
+                });
+            },
+            beforeSend: function(){
+                $('#register').html("Carregando...");
+            },
+            complete: function(){
+                $('#register').html("");
+            },
+        });
+    });
+*/
+
+});
+</script>
+<?php
+    Page::closeBody();
+?>
