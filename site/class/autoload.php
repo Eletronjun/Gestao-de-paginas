305da05d7d1b70b8f7f6f8585996f6431453792a
Iasmin Mendes <Iasmin Mendes>
2016-09-26 19:34:38 -0300

    Adding webPageException.php

--- site/class/autoload.php
+++ site/class/autoload.php
@@ -10,16 +10,17 @@
     //Model
     require_once __DIR__ . '/model/category.php';
     require_once __DIR__ . '/model/webPage.php';
-    
+
     //Html
     require_once __DIR__ . '/html/page.php';
     require_once __DIR__ . '/html/menu.php';
-    
+
     //Utilities
     require_once __DIR__ . '/utilities/session.php';
-    
+
     //Exception
     require_once __DIR__ . '/exception/pageException.php';
+    require_once __DIR__ . '/exception/webPageException.php';
     require_once __DIR__ . '/exception/sessionException.php';
     require_once __DIR__ . '/exception/databaseException.php';
     require_once __DIR__ . '/exception/categoryException.php';
