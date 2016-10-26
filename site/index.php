1e029cb55410e5d2bafe59a65efdc952cb7a9a64
mendesiasmin <mendesiasmin96@gmail.com>
2016-10-26 14:18:57 -0200

    Setting links in last publications

--- site/index.php
+++ site/index.php
@@ -16,117 +16,6 @@
 
 ?>
 
-<style>
-  .last_publications section{
-    width: 50.5rem;
-    max-width: 808px;
-  }
-  .last_publications ul {
-    position:relative;
-    height: 12.25rem;
-    width: 25rem;
-    overflow:hidden;
-    float:left;
-    background-color:#111F1E;
-    list-style: none;
-    margin-left: 0.5rem;
-    margin-bottom: 0.5rem;
-    border: solid #111F1E 0.1rem;
-    border-radius: 1rem;
-  }
-  .last_publications img {
-    position:absolute;
-    left:0;
-    bottom:-6.125rem;
-    cursor:pointer;
-    width: 100%;
-    height: auto;
-    margin: -3rem 0;
-    -webkit-transition:bottom .3s ease-in-out;
-    -moz-transition:bottom .3s ease-in-out;
-    -o-transition:bottom .3s ease-in-out;
-    transition:bottom .3s ease-in-out
-  }
-  .last_publications img.top:hover {
-    bottom:-17rem;
-    padding-top:17rem;
-  }
-  .last_publications #first ul {
-    height: 25rem;
-    width: 25rem;
-    margin: 0;
-  }
-  .last_publications #first img {
-    bottom:0;
-    margin: 0;
-  }
-  .last_publications #first img.top:hover {
-    bottom:-7rem;
-    padding-top:7rem;
-  }
-  .last_publications h2, .last_publications p{
-    margin: 0.5rem 0 0 0.5rem;
-    padding: 0;
-    color: #fff;
-  }
-  .last_publications p{
-    font-size: 0.9rem;
-    max-height: 3rem;
-    margin-right: 0.5rem;
-  }
-
-  .category_banner {
-    margin: 1.875rem 0;
-  }
-
-  .category_banner h2{
-    margin-bottom: 0.375rem;
-  }
-  .category_banner section{
-    width:45%;
-    margin-left:2.5%;
-    float:left;
-    height:20rem;
-  }
-  .category_banner div{
-    width:4.6875rem;
-    height:4.6875rem;
-    display:block;
-    float:left;
-    margin:0;
-    padding:0;
-  }
-  .category_banner img{
-    width:4.6875rem;
-    height:auto;
-  }
-  .category_banner p{
-    text-indent:0;
-    margin:0;
-    padding:0;
-    display:block;
-    float:left;
-    max-width:75%;
-    font-size:0.8rem;
-    margin-left:0.5rem;
-  }
-  .category_banner .title{
-    font-weight:bold;
-    font-size:1rem;
-  }
-  .category_banner a,
-  .category_banner a:hover,
-  .category_banner a:active,
-  .category_banner a:visited{
-    color:inherit;
-    text-decoration:none;
-    display:table;
-    width:24rem;
-    margin:0.325rem;
-    text-align:left;
-  }
-</style>
-
 <div id="content">
 
   <h1></h1>
@@ -137,48 +26,53 @@
     <section class="last_publications">
       <div id="first">
         <ul>
+          <a href="publications.php?code=<?php echo $last_publications[0][0]?>">
             <li>
                <h2><?php echo $last_publications[0][1]?></h2>
-               <?php echo $last_publications[0][2]?></p>
+               <?php
+                 if(strlen($last_publications[0][2]))
+                   echo $last_publications[0][2] . "...</p>";
+               ?>
             </li>
             <li>
-              <a href="publications.php?code=<?php echo $last_publications[0][0]?>">
                 <img class="top" src="res/file/<?php echo $last_publications[0][3]?>" alt=""/>
-              </a>
             </li>
+          </a>
         </ul>
       </div>
 
       <ul>
-        <li>
-           <h2><?php echo $last_publications[1][1]?></h2>
-           <?php echo $last_publications[1][2]?></p>
-        </li>
-        <li>
-          <a href="publications.php?code=<?php echo $last_publications[1][0]?>">
-            <img class="top" src="res/file/<?php echo $last_publications[1][3]?>" alt=""/>
-          </a>
-        </li>
+        <a href="publications.php?code=<?php echo $last_publications[1][0]?>">
+          <li>
+             <h2><?php echo $last_publications[1][1]?></h2>
+             <?php
+               if(strlen($last_publications[1][2]))
+                 echo $last_publications[1][2] . "...</p>";
+             ?>
+          </li>
+          <li>
+              <img class="top" src="res/file/<?php echo $last_publications[1][3]?>" alt=""/>
+          </li>
+        </a>
       </ul>
 
       <ul>
-        <li>
-           <h2><?php echo $last_publications[2][1]?></h2>
-           <?php echo $last_publications[1][2]?></p>
-        </li>
-        <li>
-          <a href="publications.php?code=<?php echo $last_publications[2][0]?>">
-            <img class="top" src="res/file/<?php echo $last_publications[2][3]?>" alt=""/>
-          </a>
-        </li>
+        <a href="publications.php?code=<?php echo $last_publications[2][0]?>">
+          <li>
+            <h2><?php echo $last_publications[2][1]?></h2>
+            <?php
+              if(strlen($last_publications[2][2]))
+                echo $last_publications[2][2] . "...</p>";
+            ?>
+          </li>
+          <li>
+              <img class="top" src="res/file/<?php echo $last_publications[2][3]?>" alt=""/>
+          </li>
+        </a>
       </ul>
     </section>
 
   <?php
-  /*$arr = WebPageDao::returnLast3();
-  while(list($code, $title, $content, $image) = each($arr)){
-    echo "$code $title $image<br>";
-  }*/
   echo "<div class=\"category_banner\">";
     foreach (CategoryDao::returnActiveCategories() as $code => $name) {
         $data = WebPageDao::returnLast3byCategory($code);
@@ -200,7 +94,7 @@
           }
           echo "</section>";
         }
-    }
+        }
   echo "</div>";
 echo "</div>";
 Page::footer();
