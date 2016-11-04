d65c646e96cff23b5c7c2cfd105024dc7f9b78d3
VinyPinheiro <viny-pinheiro@hotmail.com>
2016-11-04 14:05:25 -0200

    Fix error in tests

--- site/tests/model.webPageTest.php
+++ site/tests/model.webPageTest.php
@@ -10,59 +10,59 @@ namespace htmlTests{
 
     class WebPageTest extends \PHPUnit_Framework_TestCase
     {
-public function testWebPageValidWebPage()
-{
-    new WebPage("Test Page", "EletronJun", 1, "Post Test", 1, "09-01-1996", "09-01-1996");
-}
+        public function testWebPageValidWebPage()
+        {
+            new WebPage("Test Page", "EletronJun", 1, "Post Test", 1, "09-01-1996", "09-01-1996");
+        }
 
-public function testWebPageValidWebPageWithoutContentAndDates()
-{
-    new WebPage("Test Page", "EletronJun", 1, "Post Teste");
-}
-
-        /**
-         * @expectedException \exception\WebPageException
-         */
-public function testTitleInvalid()
-{
-    new WebPage("", "EletronJun", 1, "Post Teste");
-}
+        public function testWebPageValidWebPageWithoutContentAndDates()
+        {
+            new WebPage("Test Page", "EletronJun", 1, "Post Teste");
+        }
 
         /**
          * @expectedException \exception\WebPageException
          */
-public function testTitleLarger()
-{
-    new WebPage(
-        "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678900",
-        "EletronJun",
-        1,
-        "Post Teste"
-    );
-}
+        public function testTitleInvalid()
+        {
+            new WebPage("", "EletronJun", 1, "Post Teste");
+        }
 
         /**
          * @expectedException \exception\WebPageException
          */
-public function testAuthorInvalid()
-{
-    new WebPage("Page Test", "", 1, "Post Teste");
-}
+        public function testTitleLarger()
+        {
+            new WebPage(
+                "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678900",
+                "EletronJun",
+                1,
+                "Post Teste"
+            );
+        }
 
         /**
          * @expectedException \exception\WebPageException
          */
-public function testAuthorLarger()
-{
-    new WebPage("Page Test", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678900", 1, "Post Teste");
-}
+        public function testAuthorInvalid()
+        {
+            new WebPage("Page Test", "", 1, "Post Teste");
+        }
 
         /**
          * @expectedException \exception\WebPageException
          */
-public function testContentInvalid()
-{
-    new WebPage("", "EletronJun", 1, "
+        public function testAuthorLarger()
+        {
+            new WebPage("Page Test", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678900", 1, "Post Teste");
+        }
+
+        /**
+         * @expectedException \exception\WebPageException
+         */
+        public function testContentInvalid()
+        {
+            new WebPage("", "EletronJun", 1, "
             Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test
             Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test
             Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test
@@ -104,20 +104,14 @@ public function testContentInvalid()
             Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test
             Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test Post Test
             Post");
-}
+        }
 
         /**
          * @expectedException \exception\WebPageException
-         */
-public function testCategoryInvalid()
+        */
+        public function testCategoryInvalid()
         {
-<<<<<<< HEAD
             new WebPage("Page Test", "EletronJun", null, "Post Teste");
         }
-=======
-            new WebPage("Page Test", "EletronJun", NULL, "Post Teste");
-        }
-
->>>>>>> 9c241385ba02917012d5938f4cb1832ee262077d
     }
 }
