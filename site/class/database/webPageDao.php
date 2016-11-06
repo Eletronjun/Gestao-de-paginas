b3f578b208d65eb3fb0bce41dfb4aaf7b24e51ed
mendesiasmin <mendesiasmin96@gmail.com>
2016-11-06 19:03:23 -0200

    Remove suspension points in returnLast3

--- site/class/database/webPageDao.php
+++ site/class/database/webPageDao.php
@@ -201,7 +201,7 @@ namespace dao{
             for ($i = 0; $row = $resultSet->fetch_assoc(); $i++) {
                 $data[$i][0] = $row['code'];
                 $data[$i][1] = $row['title'];
-                $data[$i][2] = substr($row['content'], 0, 150) . "...";
+                $data[$i][2] = substr($row['content'], 0, 150);
                 $data[$i][3] = $row['image'];
             }
 
