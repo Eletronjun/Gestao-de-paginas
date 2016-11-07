4cd6cecb65896b89ddb57fbfb9680619c405b921
mendesiasmin <mendesiasmin96@gmail.com>
2016-11-06 23:44:52 -0200

    Create Projects page

---
+++ site/projects.php
@@ -0,0 +1,31 @@
+<?php
+    require_once __DIR__ . "/class/autoload.php";
+
+    use \utilities\Session as Session;
+    use \html\Page as Page;
+    use \html\CommunityMenu as CommunityMenu;
+    use \dao\WebPageDao as WebPageDao;
+    use \dao\CategoryDao as CategoryDao;
+    use \model\WebPage as WebPage;
+    use \configuration\Globals as Globals;
+
+    Page::header(Globals::ENTERPRISE_NAME);
+
+    $menu = new CommunityMenu();
+    $menu->construct();
+
+?>
+
+<main>
+
+  <div id="page_title">
+    <h1>Projetos</h1>
+    <img src="res/img/Circuito.png">
+  </div>
+
+</main>
+<?php
+Page::footer();
+Page::closeBody();
+
+?>
