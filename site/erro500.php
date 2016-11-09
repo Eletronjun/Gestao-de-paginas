0561e9c4ffc9ad39afa0c781d8cf8ae7d03c8f3c
mendesiasmin <mendesiasmin96@gmail.com>
2016-11-09 10:45:54 -0200

    Create page erro500.php

---
+++ site/erro500.php
@@ -0,0 +1,29 @@
+<?php
+    require_once __DIR__ . "/class/autoload.php";
+
+    use \html\Page as Page;
+    use \html\CommunityMenu as CommunityMenu;
+    use \model\WebPage as WebPage;
+    use \configuration\Globals as Globals;
+
+    Page::header(Globals::ENTERPRISE_NAME);
+
+    $menu = new CommunityMenu();
+    $menu->construct();
+
+?>
+
+<main>
+
+  <h1></h1>
+  <figure id="img_error">
+    <img src="res/img/erro500.jpg">
+  </figure>
+
+</main>
+
+<?php
+Page::footer();
+Page::closeBody();
+
+?>
