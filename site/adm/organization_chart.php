4a5f9293e8a748f626c77d0c18404797b0a31728
mendesiasmin <mendesiasmin96@gmail.com>
2016-11-28 20:43:30 -0200

    Create Organization chart page

---
+++ site/adm/organization_chart.php
@@ -0,0 +1,33 @@
+<?php
+    require_once __DIR__ . "/../class/autoload.php";
+
+    use \utilities\Session as Session;
+    use \html\Page as Page;
+    use \html\AdministratorMenu as AdministratorMenu;
+    use \configuration\Globals as Globals;
+
+    Page::startHeader("Organograma");
+    Page::styleSheet("form");
+    Page::closeHeader();
+
+    $session = new Session();
+    $session->verifyIfSessionIsStarted();
+
+    $menu = new AdministratorMenu();
+    $menu->construct();
+
+
+?>
+<main>
+  <h1></h1>
+  <section class="left" style="margin-bottom:3.125rem">
+    <h1>Gerenciar Organograma</h1>
+    <p style="font-size:1.5625rem;margin-top:-1.7rem;">Camila Ferrer</p>
+  </section>
+
+</main>
+
+<?php
+    Page::footer();
+    Page::closeBody();
+?>
