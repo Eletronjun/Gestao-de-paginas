3c2e006bf2ef54180264e5dad9477710958f959a
mendesiasmin <mendesiasmin96@gmail.com>
2016-12-09 21:55:32 -0200

    Change button remove to image

--- site/class/html/findCategory.php
+++ site/class/html/findCategory.php
@@ -69,7 +69,7 @@ namespace html{
             for ($i=0; $i < count($data); $i++) {
                 echo "<tr>\n";
                 FindCategories::getCheckboxData($data, $i);
-                echo "<td><button type='button' value='{$data[$i][0]}-_-{$data[$i][1]}' class='button_category'>Remover</button></td>";
+                echo "<td><input type='image' src='../res/img/Lixeira.png' value='{$data[$i][0]}-_-{$data[$i][1]}' class='button_category img_button' ></td>";
                 echo "</tr>\n";
             }
         }
