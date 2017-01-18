426a65aa00dd029dd10afc4eeba5643cb0d4a10a
mendesiasmin <mendesiasmin96@gmail.com>
2017-01-18 15:54:29 -0200

    Adjustments

--- site/login.php
+++ site/login.php
@@ -6,24 +6,24 @@
 
     Page::startHeader(Globals::ENTERPRISE_NAME);
     Page::styleSheet("form");
+    Page::styleSheet("login");
     Page::closeHeader();
 
     $menu = new CommunityMenu();
     $menu->construct();
 
 ?>
-    <main style="margin-top: 2rem;">
-      <img src="res/img/Etron.png" style="max-width:230px; display:block; float:left; margin-top:2rem"/>
-      <form action="controller/initSession.php" method="POST" 
-        style="width: auto; display:block; float:left; margin-left:5rem;">
+    <main>
+      <figure  id="etron"><img src="res/img/Etron.png"></figure>
+      <form action="controller/initSession.php" method="POST">
           <h1>Bem-vindo de volta!</h1>
-            <fieldset style="width:20rem">
+            <fieldset>
               <label>Email</label><br>
               <input type="text" id="email" name="email" required><br><br>
               <label>Senha</label><br>
               <input type="password" id="password" name="password" required><br><br>
             </fieldset>
-          <input type="submit" value="Logar" style="margin-top: 1.875rem"/>
+          <input type="submit" value="Logar"/>
       </form>
   </main>
 <?php
