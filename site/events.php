3280a9af0e41a496697e9097da5155aa655e2b43
mendesiasmin <mendesiasmin96@gmail.com>
2017-01-19 12:16:16 -0200

    Change layout to multiple collumns

--- site/events.php
+++ site/events.php
@@ -28,58 +28,36 @@
   <section id="courses">
     <h3>Cursos</h3>
     <ul>
-      <div class="flex">
-        <div class="set_flex padding_right">
-          <div class="left">
-          <li>
-            <figure><img src="res/img/Arduino.png"></figure>
-
-            <h4>Arduino</h4>
-            <p>Neste curso será ensinado conceitos básicos das placas de Arduino, não sendo necessário ter nenhum conhecimento prévio do assunto.
-            </p>
-          </li>
-          <li>
-            <figure><img src="res/img/C.png"></figure>
-
-            <h4>C Básico</h4>
-            <p>Este curso tem como objetivo apresentar os conceitos básicos da linguagem C, uma das linguagens de programação mais conhecidas.
-            </p>
-          </li>
-        </div>
-      </div>
-      <div class="set_flex padding_left">
-        <div class="right">
-          <li>
-            <figure><img src="res/img/Java.png"></figure>
-            <h4>Java</h4>
-            <p>O objetivo deste curso é introduzir conceitos básicos de Java, linguagem de programação orientada a objetos.
-            </p>
-          </li>
-          <li>
-            <figure><img src="res/img/C.png"></figure>
-            <h4>C Avançado</h4>
-            <p>Este curso tem como objetivo ensinar técnicas e conceitos mais avançados da linguagem de programação C.
-            </p>
-          </li>
-        </div>
-      </div>
-    </div>
-  <div class="flex">
-      <div class="set_flex padding_right">
-        <div class="left">
-          <li>
-            <figure><img src="res/img/VHDL.png"></figure>
-            <h4>VHDL</h4>
-            <p>Curso voltado a programação VHDL, introduzindo conceitos desde portas combinacionais à ULAs.<br>
-            </p>
-          </li>
-        </div>
-      </div>
-      <div class="set_flex padding_left">
-        <div class="right">
-        </div>
-      </div>
-  </div>
+        <li>
+          <figure><img src="res/img/Arduino.png"></figure>
+          <h4>Arduino</h4>
+          <p>Neste curso será ensinado conceitos básicos das placas de Arduino, não sendo necessário ter nenhum conhecimento prévio do assunto.
+          </p>
+        </li>
+        <li>
+          <figure><img src="res/img/C.png"></figure>
+          <h4>C Básico</h4>
+          <p>Este curso tem como objetivo apresentar os conceitos básicos da linguagem C, uma das linguagens de programação mais conhecidas.
+          </p>
+        </li>
+        <li>
+          <figure><img src="res/img/VHDL.png"></figure>
+          <h4>VHDL</h4>
+          <p>Curso voltado a programação VHDL, introduzindo conceitos desde portas combinacionais à ULAs.<br>
+          </p>
+        </li>
+        <li>
+          <figure><img src="res/img/Java.png"></figure>
+          <h4>Java</h4>
+          <p>O objetivo deste curso é introduzir conceitos básicos de Java, linguagem de programação orientada a objetos.
+          </p>
+        </li>
+        <li>
+          <figure><img src="res/img/C.png"></figure>
+          <h4>C Avançado</h4>
+          <p>Este curso tem como objetivo ensinar técnicas e conceitos mais avançados da linguagem de programação C.
+          </p>
+        </li>
     </ul>
   </section>
 
