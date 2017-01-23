bdbbbaeed9ee2733a62a544f287db6bda7caf4a6
mendesiasmin <mendesiasmin96@gmail.com>
2017-01-23 12:16:11 -0200

    Returning 115 characters in returnLast3ByCategory function description

--- site/class/database/webPageDao.php
+++ site/class/database/webPageDao.php
@@ -231,7 +231,7 @@ namespace dao{
             for ($i = 0; $row = $resultSet->fetch_assoc(); $i++) {
                 $data[$i][0] = $row['code'];
                 $data[$i][1] = $row['title'];
-                $data[$i][2] = substr($row['content'], 0, 100);
+                $data[$i][2] = substr($row['content'], 0, 115);
                 $data[$i][3] = $row['image'];
                 $data[$i][4] = $row['creation_date'];
                 $data[$i][5] = $row['video'];
